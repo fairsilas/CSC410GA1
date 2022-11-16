b18345d5b572a15797c3e3178c22c28fd5d15866
Silas Fair <78548914+SilasFair121221@users.noreply.github.com>
2022-11-16 11:15:10 -0500

    added GA2 implamentation

--- GA project/main.cpp
+++ GA project/main.cpp
@@ -8,8 +8,12 @@ using namespace std;  // we use this so that we do not have to have
 
 int main() {
     genome myGenome;
+    myGenome.set_mRate(0.5);
     myGenome.allocate(4); //allocates an array of 4 pixels in myGenome
     myGenome.randomize();
     myGenome.print();
+    myGenome.set_pixel(2,{100,100,100});
+    myGenome.print();
+    myGenome.calculate_overall_fitness({ 100,100,100 }, 4);
     return 0;
 }
