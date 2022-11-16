293a6ef44a42fd24dafcbe16673b325df8c3e72c
Silas Fair <78548914+SilasFair121221@users.noreply.github.com>
2022-11-16 11:11:18 -0500

    Updated to GA2

--- GA project/genome.h
+++ GA project/genome.h
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-class Pixel {
-public:
+struct Pixel {
     int red;
     int blue;
     int green;
@@ -33,6 +32,89 @@ public:
         genes = new Pixel[numGenes];
         nGenes = numGenes;
     }
+    //set mutation rate
+    void set_mRate(double val) {
+        if (val >= 0 && val <= 1) {
+            mRate = val;
+        }
+
+    }
+
+    //return the mutation rate
+    double get_mRate() {
+        return mRate;
+    }
+
+    // mutate a single gene
+    void mutate_gene(int index) 
+    {
+        double min = 0;
+        double max = 1;
+        // if random 0-1 is less than mutation rate then set colors to random
+        //mutate red
+        double random = (double)rand() / 1;
+        random = min + random * (max - min);
+        if (random < mRate)
+        {
+            int redValue = (rand() % 226) + 1;
+            set_red(index, redValue);
+        }
+        //mutate green
+        random = (double)rand() / 1;
+        random = min + random * (max - min);
+        if (random < mRate)
+        {
+            int greenValue = (rand() % 226) + 1;
+            set_green(index, greenValue);
+        }
+        //mutate blue
+        random = (double)rand() / 1;
+        random = min + random * (max - min);
+        if (random < mRate)
+        {
+            int blueValue = (rand() % 226) + 1;
+            set_blue(index, blueValue);
+        }
+    }
+
+    // mutate the all genes
+    void mutate() {
+        for (int i = 0; i < nGenes; i++) {
+            mutate_gene(i);
+        }
+    }
+
+    // calculate gene fitness 
+    double calculate_gene_fitness(int index, Pixel targetPixel) {
+        float percent_red_error = abs(get_red(index) - targetPixel.red)/ double(226);
+        double percent_green_error = abs(get_green(index) - targetPixel.green) / double(226);
+        double percent_blue_error = abs(get_blue(index) - targetPixel.blue) / double(226);
+        double gene_fitness = (percent_red_error + percent_green_error + percent_blue_error) / double(3);
+        return gene_fitness;
+    }
+
+    //calculate overall fitness
+    double calculate_overall_fitness(Pixel target, int nPixels) {
+        double fitness_total = 0;
+        for (int i = 0; i < nPixels; i++) {
+            fitness_total += calculate_gene_fitness(i, target);
+        }
+        fitness_total /= nPixels;
+        return fitness_total;
+    }
+
+    //set pixel
+    void set_pixel(int index, Pixel newPixel) {
+        if ((index <= nGenes) && (index >= 0)) {
+            genes[index] = newPixel;
+        }
+        else {
+            cout << "Out of range line 108-110 is bad";
+        }
+        
+        
+    }
+    
     void deallocate()
     {
         if (genes == NULL)
@@ -80,7 +162,9 @@ public:
     {
         return genes[index].blue;
     }
-    void print()
+
+    //print out all the genes color values
+    void print() 
     {
         for (int i = 0; i < nGenes; i++)
         {
@@ -90,6 +174,7 @@ public:
 
     Pixel* genes;
     int nGenes;
+    double mRate = 0;
 private:
 
 };
